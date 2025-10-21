3198ed7d03c7a6f9b3d99bb6c64ace06dcdab5de
xondamir00 <ergashevxondamir70@gmail.com>
2025-10-21 18:07:36 +0500

    add auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/page/home";
+import Login from "./components/auth/login";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Login />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/auth/login.tsx
@@ -0,0 +1,51 @@
+import { Link, useNavigate } from "react-router-dom";
+import { Input } from "../ui/input";
+
+const Login = () => {
+  const navigate = useNavigate("");
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <form className="bg-white p-6 w-full max-w-sm rounded-lg shadow-lg">
+        <p className="text-xl font-semibold text-center text-black mb-4">
+          Sign in to your account
+        </p>
+
+        <div className="mb-4">
+          <Input
+            className="py-6"
+            type="number"
+            placeholder="Enter phone number"
+          />
+        </div>
+
+        <div className="mb-4">
+          <Input
+            className="py-6"
+            type="passwwor"
+            placeholder="Enter password"
+          />
+        </div>
+
+        <Link to={"/"}>
+          <button
+            type="submit"
+            className="w-full py-3 bg-indigo-600 text-white text-sm font-medium uppercase rounded-lg hover:bg-indigo-700 transition-colors"
+          >
+            Sign in
+          </button>
+        </Link>
+        <p className="text-gray-500 text-sm text-center mt-4">
+          No account?{" "}
+          <a
+            href="#"
+            className="text-indigo-600 underline hover:text-indigo-800"
+          >
+            Sign up
+          </a>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
