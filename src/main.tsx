2dd7265575b4db8166c362ad112342ef2bbb8f6f
czargrozniy444 <czargrozniy444@gmail.com>
2025-10-23 18:15:18 +0500

    addedd modeToggle i18react profile logout

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./themeProvider/theme-provider.tsx";
+import "./i18react/i18react.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
-    </BrowserRouter>
+     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App/>
+    </ThemeProvider>
+        </BrowserRouter>
   </StrictMode>
 );
--- src/shared/logo.tsx
+++ src/shared/logo.tsx
@@ -1,9 +1,14 @@
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 const Logo = () => {
+  const { t } = useTranslation();
+
   return (
     <Link to={"/"} className="flex text-white items-center space-x-2">
-      <div className="text-3xl font-spaceGrotesk font-bold ">CRM</div>
+      <div className="text-3xl font-spaceGrotesk font-bold">
+        {t("welcomeCRM")}
+      </div>
     </Link>
   );
 };
