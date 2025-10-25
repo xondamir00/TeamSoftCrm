0b282810ae48b71e39ba9265a98f4380633916fa
xumora11 <xumora377@gmail.com>
2025-10-25 17:22:53 +0500

    pupils

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,21 +1,21 @@
 export const pupils=[
     {
         id:"01",
-        name:"sdafds",
+        name:"test",
         number:"1234235",
         number2:"2314234",
         payment:"325 000"
     },
       {
         id:"02",
-        name:"sdafds",
+        name:"test2",
         number:"1234235",
         number2:"2314234",
         payment:"325 000"
     },
       {
         id:"03",
-        name:"sdafds",
+        name:"test3",
         number:"1234235",
         number2:"2314234",
         payment:"325 000"
