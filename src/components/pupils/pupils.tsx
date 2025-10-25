8d9ad90a6a09936cafe777a9afbcda04efd8bb0a
xumora11 <xumora377@gmail.com>
2025-10-25 17:36:08 +0500

    pupilsPage

--- src/components/pupils/pupils.tsx
+++ src/components/pupils/pupils.tsx
@@ -13,29 +13,35 @@ import { useTranslation } from "react-i18next";
 const Pupils = () => {
   const { t } = useTranslation();
   return (
-    <Table>
-      <TableCaption>{t("pupils")}</TableCaption>
-      <TableHeader>
-        <TableRow className="cursor-pointer">
-           <TableHead>ID</TableHead>
-          <TableHead className="w-[100px]">{t("name")}</TableHead>
-          <TableHead>{t("phone")}</TableHead>
-          <TableHead>{t("phone2")}</TableHead>
-          <TableHead className="text-right">{t("price")}</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody className="cursor-pointer">
-        {pupils.map((item) => (
-          <TableRow key={item.id}>
-            <TableCell className="font-medium">{item.id}</TableCell>
-            <TableCell className="font-medium">{item.name}</TableCell>
-            <TableCell>{item.number}</TableCell>
-            <TableCell>{item.number2}</TableCell>
-            <TableCell className="text-right">{item.payment}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <div className="w-full bg-white rounded-2xl">
+      <div className="md:block overflow-x-auto">
+        <Table>
+          <TableCaption>{t("pupils")}</TableCaption>
+
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead className="w-[150px]">{t("name")}</TableHead>
+              <TableHead>{t("phone")}</TableHead>
+              <TableHead>{t("phone2")}</TableHead>
+              <TableHead className="text-right">{t("price")}</TableHead>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+            {pupils.map((item) => (
+              <TableRow key={item.id}>
+                <TableCell className="font-medium">{item.id}</TableCell>
+                <TableCell className="font-medium">{item.name}</TableCell>
+                <TableCell>{item.number}</TableCell>
+                <TableCell>{item.number2}</TableCell>
+                <TableCell className="text-right">{item.payment}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+      </div>
   );
 };
 
