3f2e34f0c05a3e401efcf048dfc6d1c294b0687b
programmer277 <you@example.com>
2025-11-01 16:57:02 +0500

    save local changes before pulling main

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,45 @@
-<<<<<<< HEAD
 import { Route, Routes } from "react-router-dom"
 import Login from "./components/auth/login"
 import Homelayout from "./components/layout/homelayout"
 import AdminLayout from "./components/layout/adminlayout"
+import Teacherlayout from "./components/layout/teacherlayout"
+
 import Home from "./components/page/home"
 import Students from "./components/page/Adminpage/StudentsPage"
 import Teachers from "./components/page/Adminpage/TeachersPage"
 import Groups from "./components/page/Adminpage/GroupsPage"
-
-
-const App = () => {
-  return (
-    <Routes>
-      {/* Public sahifalar */}
-      <Route path="/" element={<Homelayout />}>
-        <Route index element={<Home />} />
-      </Route>
-=======
-import { Route, Routes } from "react-router-dom";
-import Login from "./components/auth/login";
-import Homelayout from "./components/layout/homelayout";
-import Home from "./components/page/home";
-import Teacherlayout from "./components/layout/teacherlayout";
-import Teacher from "./components/page/Teacher";
-import GgroupDetail from "./components/teacher/GgroupDetail";
-import Pupils from "./components/pupils/pupils";
+import Teacher from "./components/page/Teacher"
+import GgroupDetail from "./components/teacher/GgroupDetail"
+import Pupils from "./components/pupils/pupils"
 
 const App = () => {
   return (
     <div className="light:bg-[#f2f2f2] h-screen dark:bg-gray-900">
       <Routes>
+        {/* Public sahifalar */}
         <Route path="/" element={<Homelayout />}>
           <Route index element={<Home />} />
           <Route path="student" element={<Pupils />} />
         </Route>
+
+        {/* Login sahifasi */}
+        <Route path="/auth" element={<Login />} />
         <Route path="/sign" element={<Login />} />
+
+        {/* Teacher sahifalari */}
         <Route path="/teacher" element={<Teacherlayout />}>
-          <Route index element={<Teacher />} />\
+          <Route index element={<Teacher />} />
           <Route path=":id" element={<GgroupDetail />} />
+        </Route>
+
+        {/* Admin panel sahifalari */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="students" element={<Students />} />
+          <Route path="teachers" element={<Teachers />} />
+          <Route path="groups" element={<Groups />} />
         </Route>
       </Routes>
     </div>
-  );
-};
->>>>>>> fbff06d5b5e5f21fbebb7b8f12b72405f048b17a
-
-      <Route path="/auth" element={<Login />} />
-
-      {/* Admin panel sahifalari */}
-      <Route path="/admin" element={<AdminLayout />}>
-        <Route path="students" element={<Students />} />
-        <Route path="teachers" element={<Teachers />} />
-        <Route path="groups" element={<Groups />} />
-      </Route>
-    </Routes>
   )
 }
 
