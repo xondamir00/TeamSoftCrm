aab68e074fdc25f60228759d034d11234f1bc91c
Abdusamatov01 <abdusamatovdilshod233@gmail.com>
2025-11-02 17:23:04 +0500

    update

--- src/components/pupils/pupils.tsx
+++ src/components/pupils/pupils.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Table,
   TableBody,
@@ -19,8 +19,6 @@ import {
   Drawer,
   DrawerClose,
   DrawerContent,
-  DrawerHeader,
-  DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
 import { Button } from "@/components/ui/button";
