cebd7eb219a4f1df00a87fcd46d6ddfc469cfcde
xondamir00 <ergashevxondamir70@gmail.com>
2025-12-09 17:48:10 +0500

    file error fixed

--- src/page/Teacher.tsx
+++ src/page/Teacher.tsx
@@ -1,13 +1,11 @@
 import TeacherGroup from "@/Page/TeacherGroup";
 
-const Teacher: React.FC = () => {
-  return (
-    <div className="p-6 min-h-screen">
-      <div className="mt-6">
-        <TeacherGroup />
-      </div>
+const Teacher = () => {
+  <div className="p-6 min-h-screen">
+    <div className="mt-6">
+      <TeacherGroup />
     </div>
-  );
+  </div>;
 };
 
 export default Teacher;
